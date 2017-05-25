08c89f1f70fb15615db569f19fd42473be346d28
Chris Arcand <chris@chrisarcand.com>
2017-05-24 21:31:05 -0500

    Fix deps because Go is terrible

--- Formula/miqversions.rb
+++ Formula/miqversions.rb
@@ -1,5 +1,5 @@
-require 'formula'
-require 'language/go'
+require "formula"
+require "language/go"
 
 class Miqversions < Formula
   desc "Because naming is hard. Really, really hard."
@@ -7,18 +7,25 @@ class Miqversions < Formula
   url "https://github.com/chrisarcand/miqversions/archive/v1.0.0-pre1.tar.gz"
   sha256 "8ed8d32e8e0046ed0208919089c81f60a00f477d40e819415c5be3ea6dcae42e"
 
-  depends_on "go" => :build
+  head "https://github.com/chrisarcand/miqversions.git"
 
   go_resource "github.com/olekukonko/tablewriter" do
     url "https://github.com/olekukonko/tablewriter.git",
       :revision => "febf2d34b54a69ce7530036c7503b1c9fbfdf0bb"
   end
 
+  go_resource "github.com/mattn/go-runewidth" do
+    url "https://github.com/mattn/go-runewidth.git",
+      :revision => "97311d9f7767e3d6f422ea06661bc2c7a19e8a5d"
+  end
+
+  depends_on "go" => :build
+
   def install
     ENV["GOPATH"] = buildpath
 
     # set up package
-    (buildpath/"src/github.com/chrisarcand/miqversions").install Dir["*"]
+    (buildpath/"src/github.com/chrisarcand/miqversions").install Dir["{*,.git}"]
 
     # set up dependencies
     Language::Go.stage_deps resources, buildpath/"src"
@@ -29,7 +36,7 @@ class Miqversions < Formula
       "build",
       "-o", bin/"miqversions",
       "-v",
-      "github.com/chrisarcand/miqversions",
+      "github.com/chrisarcand/miqversions"
     )
   end
 end
